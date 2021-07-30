5748c264fb0f5286d5bebec46fd7941ec65b08af
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-07-30 15:24:06 +0100

    setState of currentPage to page and add active class to pagination

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -3,20 +3,19 @@ import _ from 'lodash';
 
 const Pagination = (props) => {
 
-  const {itemsCount, pageSize} = props;
+  const {itemsCount, pageSize, currentPage, onPageChange} = props;
+  console.log(currentPage)
   const pagesCount = Math.ceil(itemsCount / pageSize);
  
   if (pagesCount === 1) return null;
 
   const pages = _.range(1, pagesCount + 1);
 
-
-
   return <nav>
       <ul className="pagination">
       { pages.map(page =>
-        <li key={page} className="page-item">
-          <a className="page-link">{page}</a>
+        <li key={page} className={ page === currentPage ? 'page-item active' : 'page-item' }>
+          <a onClick={() => onPageChange(page)} className="page-link">{page}</a>
         </li>
       ) }
       </ul>
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,7 +6,8 @@ import Pagination from './common/pagination';
 class Movies extends Component {
   state = {
     movies: getMovies(),
-    pageSize: 4
+    pageSize: 4,
+    currentPage: 1
   }
   
   handleDelete = (movie) => {
@@ -23,12 +24,17 @@ class Movies extends Component {
     this.setState({ movies })
   }
 
+  // Whenever the state of a component is changed
+  // The component and its children are re-rendered
+
   handlePageChange = (page) => {
-    console.log(page)
+    this.setState({ currentPage: page })
   }
 
   render() {
     const { length: moviesCount } = this.state.movies
+    const { pageSize, currentPage } = this.state
+
     if (moviesCount === 0) return <p>There are no movies in the database</p>
     
     return (
@@ -64,7 +70,8 @@ class Movies extends Component {
         </table>
         <Pagination
           itemsCount={moviesCount}
-          pageSize={10}
+          pageSize={pageSize}
+          currentPage={currentPage}
           onPageChange={this.handlePageChange} />
       </React.Fragment>
      );
