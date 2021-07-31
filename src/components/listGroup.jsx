e2ee028b7ce98c6a21e60259bcb94762a4c939ec
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-07-31 20:53:08 +0100

    Rename listGroup to appropriate genres

--- src/components/listGroup.jsx
+++ src/components/listGroup.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
+import { genres } from '../services/fakeGenreService';
 
 class ListGenre extends Component {
   state = {  }
   render() { 
-    return ( 
-      <ul class="list-group">
+    return (
 
-        <li class="list-group-item">Cras justo odio</li>
-        <li class="list-group-item">Dapibus ac facilisis in</li>
-        <li class="list-group-item">Morbi leo risus</li>
-        <li class="list-group-item">Porta ac consectetur ac</li>
-        <li class="list-group-item">Vestibulum at eros</li>
+      <ul class="list-group">
+        {genres.map(genre =>
+          <li class="list-group-item">{genre.name}</li>
+        )}
       </ul>
      );
   }
