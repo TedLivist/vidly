efedb44c9c1fcaceb6b3897c607b06a14932a609
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-08-01 16:07:24 +0100

    Re-add ListGroup UI Component

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -35,8 +35,17 @@
 
 import React from 'react';
 
-const ListGroup = () => {
-  return null
+const ListGroup = (props) => {
+
+  const { items, valueProperty, textProperty } = props
+
+  return (
+    <ul className="list-group">
+      { items.map(item =>
+        <li className="list-group-item" key={item[valueProperty]}>{item[textProperty]}</li>
+      ) }
+    </ul>
+  )
 }
  
 export default ListGroup;
