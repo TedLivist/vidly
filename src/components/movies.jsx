3616356827d0ff5b62cae1e90a9b633a6aaf226b
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-08-08 08:09:03 +0100

    Raise sort event

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -15,7 +15,7 @@ class Movies extends Component {
   }
 
   componentDidMount() {
-    const genres = [{ name: 'All Genres' }, ...getGenres()]
+    const genres = [{ _id: "", name: 'All Genres' }, ...getGenres()]
     this.setState({ movies: getMovies(), genres })
   }
   
@@ -41,6 +41,10 @@ class Movies extends Component {
 
   handleGenreSelect = genre => {
     this.setState({selectedGenre: genre, currentPage: 1})
+  };
+
+  handleSort = path => {
+    console.log(path)
   }
 
   render() {
@@ -68,7 +72,8 @@ class Movies extends Component {
             <MoviesTable
               movies={movies}
               onLike={this.handleLike}
-              onDelete={this.handleDelete}/>
+              onDelete={this.handleDelete}
+              onSort={this.handleSort}/>
             <Pagination
               itemsCount={filtered.length}
               pageSize={pageSize}
